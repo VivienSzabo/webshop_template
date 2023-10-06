07d4e76f9b2aa680b4cf43e5f8734c216f85ff63
unknown <vivienkorosiszabo@gmail.com>
2023-10-06 11:39:16 +0200

    created single Instrument page

--- src/pages/InstrumentSinglePage.js
+++ src/pages/InstrumentSinglePage.js
@@ -1,8 +1,56 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-export function InstrumentSinglePage() {
+export default function InstrumentSinglePage(props) {
+  //const id = props.match.params.hangszerId;
+  const param = useParams();
+  const id = param.hangszerId;
+
+  const [instrument, setInstrument] = useState({});
+  const [isPending, setPending] = useState(false);
+
+  useEffect(() => {
+    setPending(true);
+    (async () => {
+      try {
+        const res = await fetch(
+          `${process.env.REACT_APP_INSTRUMENT_URL}/api/instruments/${id}`,
+          { credentials: "include" }
+        );
+        const hangszer = await res.json();
+        setInstrument(hangszer);
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setPending(false);
+      }
+    })();
+  }, [id]);
+
   return (
-    <div>InstrumentSinglePage</div>
-  )
+    <div className="p-5  m-auto text-center content bg-lavender">
+      {isPending || !instrument.id ? (
+        <div className="spinner-border"></div>
+      ) : (
+        <div className="card p-3">
+          <div className="card-body">
+            <h4>{instrument.brand}</h4>
+            <h5 className="card-title">{instrument.name}</h5>
+            <div className="lead">{instrument.price} ft</div>
+            <p>Készleten: {instrument.quantity} db</p>
+            <img
+              className="img-fluid rounded"
+              style={{ maxHeight: "500px" }}
+              src={
+                instrument.imageURL
+                  ? instrument.imageURL
+                  : "https://via.placeholder.com/400x800"
+              }
+              alt={instrument.name}
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
-
